81610d07999cffbea551db0e285a74fd0db93a8e
etaoko333 <131201117+etaoko333@users.noreply.github.com>
2025-03-05 20:07:05 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,34 +1,23 @@
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// Handle uncaught exceptions
 process.on('uncaughtException', err => {
   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
 });
 
+// Load environment variables
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
-
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log('DB connection successful!'));
-
+// Start the server
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
+// Handle unhandled promise rejections
 process.on('unhandledRejection', err => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
   console.log(err.name, err.message);
