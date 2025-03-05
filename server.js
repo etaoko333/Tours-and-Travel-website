f703d048cfca116b10a2c4a53f59e921aed295be
etaoko333 <131201117+etaoko333@users.noreply.github.com>
2025-03-05 21:50:07 +0000

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
